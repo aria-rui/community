96d01c9eb562099e1324135ef4bee601f5b4704f
Aria <1157852679@qq.com>
2023-08-17 01:06:07 +0900

    首次提交

---
+++ src/main/java/com/JPlearner/community/CommunityApplication.java
@@ -0,0 +1,15 @@
+package com.JPlearner.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan({"com.JPlearner.community.dao"})
+
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
